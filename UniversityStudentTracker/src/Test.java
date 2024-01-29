58dad61b931a03653a58b960da50f14e7907bdf2
Arshia <154830026+arshiashirzad@users.noreply.github.com>
2024-01-29 23:53:41 +0330

    Complete app and fix bugs

--- UniversityStudentTracker/src/Test.java
+++ UniversityStudentTracker/src/Test.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Test {
     public static void main(String[] args){
         FileHandler fileHandler = new FileHandler();
+        System.out.println("Analyzing datas...");
         ArrayList<Student> students = fileHandler.Read("StData.txt");
         Classificator sorter = new Classificator();
         sorter.sort(students);
